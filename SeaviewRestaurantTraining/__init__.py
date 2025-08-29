4f51edd77ad96d1ba1fbd02754ce1efc885ab2ca
ParthP22 <parthpatel0422@gmail.com>
2025-08-29 13:27:08 -0400

    refactor: registered all current Blueprints in __init__.py

--- SeaviewRestaurantTraining/__init__.py
+++ SeaviewRestaurantTraining/__init__.py
@@ -6,9 +6,15 @@ def create_app():
     # Import blueprints
     from .announcement.__init__ import announcement_bp
     from .auth.__init__ import session_handling_bp
+    from .employee.__init__ import employee_bp
+    from .manager.__init__ import manager_bp
+    from .quiz.__init__ import quiz_bp
 
     # Register blueprints
     app.register_blueprint(announcement_bp)
     app.register_blueprint(session_handling_bp)
+    app.register_blueprint(employee_bp)
+    app.register_blueprint(manager_bp)
+    app.register_blueprint(quiz_bp)
 
     return app
