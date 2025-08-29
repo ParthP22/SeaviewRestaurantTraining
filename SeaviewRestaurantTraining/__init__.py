af139562e7b8353081d42649f651e543103a79b4
ParthP22 <parthpatel0422@gmail.com>
2025-08-29 18:47:22 -0400

    refactor: updated __init__.py with new Blueprint additions/changes

--- SeaviewRestaurantTraining/__init__.py
+++ SeaviewRestaurantTraining/__init__.py
@@ -1,18 +1,27 @@
+import secrets
 from flask import Flask
 
+def create_secret_key(length=32):
+    return secrets.token_hex(length)
+
 def create_app():
+    
     app = Flask(__name__)
+    
+    app.secret_key = create_secret_key()
 
     # Import blueprints
-    from .announcement.__init__ import announcement_bp
-    from .auth.__init__ import session_handling_bp
-    from .employee.__init__ import employee_bp
-    from .manager.__init__ import manager_bp
-    from .quiz.__init__ import quiz_bp
+    from .homepage import welcome_bp
+    from .announcement import announcement_bp
+    from .auth import auth_bp
+    from .employee import employee_bp
+    from .manager import manager_bp
+    from .quiz import quiz_bp
 
     # Register blueprints
+    app.register_blueprint(welcome_bp)
     app.register_blueprint(announcement_bp)
-    app.register_blueprint(session_handling_bp)
+    app.register_blueprint(auth_bp)
     app.register_blueprint(employee_bp)
     app.register_blueprint(manager_bp)
     app.register_blueprint(quiz_bp)
