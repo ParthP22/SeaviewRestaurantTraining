a9438cc79dcfd18100f20e719446349097079ab6
ParthP22 <parthpatel0422@gmail.com>
2025-08-29 12:24:41 -0400

    feat: created dashboard.py under manager directory to handle redirecting to manager's dashboard upon login

---
+++ SeaviewRestaurantTraining/manager/dashboard.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, url_for, session, request
+import SeaviewRestaurantTraining.quiz.certificate as certificate
+import database
+from routes import website
+from . import manager_bp
+
+@manager_bp.route('/dashboard', methods=['GET', 'POST'])
+def authenticate_user():
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    account = None
+    cursor = database.conn.cursor()
+    if 'logged_in' in session and session['logged_in']:
+        cursor.execute('SELECT * FROM Users WHERE Username=? AND Password=?', (session['username'], session['password']))
+        account = cursor.fetchone()
+        if account and account[6] == 1:
+            return render_template('manager/manager-dashboard.html')
+
+    # Show the login form with message (if any)
+    return redirect(url_for('login'))
