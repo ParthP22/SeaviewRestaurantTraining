99f2287acb92fcdbdbb34877eb10f7c4e968a96c
ParthP22 <parthpatel0422@gmail.com>
2025-09-03 18:06:37 -0400

    refactor: removed unnecessary comments and added new necessary comments. Also, removed an unnecessary try-block from the logout route

--- SeaviewRestaurantTraining/auth/login.py
+++ SeaviewRestaurantTraining/auth/login.py
@@ -8,22 +8,31 @@ import database
 from . import auth_bp
 from SeaviewRestaurantTraining.enums import AccountStatus, Role
 
+# Simply shows the login page
 @auth_bp.route('/show-login')
 def show_login():
     return render_template('login.html')
+
+# Handles the actual login functionality once the form
+# on the login page is submitted
 @auth_bp.route('/login', methods=['GET','POST'])
 def login():
     # Initialize message variable
     msg = ''
+
     # Create variables for easy access
     username = request.form['username']
     password = request.form['password']
+
     # Check if account exists using SQLite
     cursor = database.conn.cursor()
     cursor.execute('SELECT * FROM Users WHERE Username=? AND Password=?', (username, password,))
     account = cursor.fetchone()
+
     # If account exists in the database
     if account:
+
+        # Update all session variables with the new session
         session['id'] = account[0]
         session['logged_in'] = True
         session['username'] = username
@@ -31,135 +40,49 @@ def login():
         session['role'] = account[6]
         session['restricted'] = account[8]
 
+        # If the account is restricted, then return to login page with
+        # message.
         if session['restricted'] == AccountStatus.RESTRICTED.value:
             msg = 'Account is restricted'
             return render_template('login.html', msg=msg)
 
-        # After successful login, redirect to dashboard
+        # After successful login, redirect to manager dashboard if
+        # the user is a manager
         if session['role'] == Role.MANAGER.value:
             return redirect(url_for('manager.authenticate_manager'))
+        # Else, the user is an employee, so redirect to employee dashboard
         else:
             return redirect(url_for('employee.authenticate_employee'))
+        
+    # Else, the account doesn't exist, so return to login page
     else:
         msg = 'Incorrect username/password!'
         return render_template('login.html', msg=msg)
 
+# Handle log out functionality
 @auth_bp.route('/logout', methods=['GET', 'POST'])
 def logout():
+    
+    # Clear all the variables that were set in the session
     session.pop('logged_in', None)
     session.pop('username', None)
     session.pop('password', None)
     session.pop('id', None)
     session.pop('role', None)
     session.pop('restricted', None)
-    try:
-        return redirect(url_for('homepage.welcome'))
-    except Exception as e:
-        print("Couldn't load welcome page: " + e)
 
-# def render_employee_dashboard(account, cursor):
-#     cursor.execute('SELECT NUM_CORRECT, NUM_INCORRECT, MAX(ATTEMPT_NUMBER) '
-#                    'FROM ATTEMPT_HISTORY_LOG '
-#                    'WHERE EMPLOYEE_ID=? AND QUIZ_ID IN (SELECT QUIZ_ID FROM QUIZZES WHERE IS_DELETED=0)'
-#                    'GROUP BY QUIZ_ID',
-#                    (account[0],))
+    # Return to the welcome page
+    return redirect(url_for('homepage.welcome'))
 
-#     recent_attempts = cursor.fetchall()
-#     total_correct = 0
-#     total_questions = 0
-
-#     cursor.execute('SELECT TOTAL_QUESTIONS FROM QUIZZES WHERE IS_DELETED IS NOT 1 ')
-#     query = cursor.fetchall()
-
-#     if query is not None:
-#         for row in query:
-#             total_questions += row[0]
-#     # cursor.execute('SELECT QUIZ_ID FROM QUIZZES')
-#     # total_quizzes = cursor.fetchall()
-#     # total_correct = 0
-#     # total_questions = 0
-#     # if total_quizzes is not NoneType:
-#     #     for quiz in total_quizzes:
-#     #         cursor.execute(
-#     #             'SELECT NUM_CORRECT, NUM_INCORRECT, MAX(ATTEMPT_NUMBER) FROM ATTEMPT_HISTORY_LOG WHERE EMPLOYEE_ID=? AND QUIZ_ID=?',
-#     #             (account[0], quiz[0]))
-#     #         progress = cursor.fetchone()
-#     #         total_correct += progress[0]
-#     #         total_questions += progress[0] + progress[1]
-
-
-#     if recent_attempts is not None:
-#         for attempt in recent_attempts:
-#             total_correct += attempt[0]
-
-#     if total_questions == 0:
-#         percent = 0
-#     else:
-#         percent = total_correct / total_questions * 100
-#         percent = round(percent, 2)
-
-#     cursor.execute('SELECT * FROM QUIZZES WHERE QUIZ_ID NOT IN '
-#                    '(SELECT DISTINCT QUIZ_ID FROM ATTEMPT_HISTORY_LOG WHERE IS_COMPLETED=1 AND EMPLOYEE_ID=?) ', (session['id'],))
-#     quizzes = cursor.fetchall()
-
-#     cursor.execute('SELECT QUIZ_ID, QUIZ_NAME FROM QUIZZES WHERE IS_DELETED IS NOT 1 ')
-
-#     quiz_list = []
-
-#     for _, quiz in cursor.fetchall():
-#         if quiz is not None:
-#             quiz_list.append(quiz)
-
-
-
-#     cursor.execute('SELECT NUM_CORRECT, NUM_INCORRECT, MAX(ATTEMPT_NUMBER) '
-#                    'FROM ATTEMPT_HISTORY_LOG '
-#                    'WHERE EMPLOYEE_ID=? AND QUIZ_ID IN (SELECT DISTINCT QUIZ_ID FROM QUIZZES WHERE IS_DELETED IS NOT 1) '
-#                    'GROUP BY QUIZ_ID ', (session['id'],))
-
-#     num_correct = []
-#     num_incorrect = []
-#     for correct, incorrect, _ in cursor.fetchall():
-#         if correct is not None:
-#             num_correct.append(correct)
-#         else:
-#             num_correct.append(0)
-#         if incorrect is not None:
-#             num_incorrect.append(incorrect)
-#         else:
-#             num_incorrect.append(0)
-
-#     current_datetime = datetime.datetime.now()
-#     formatted_current_datetime = current_datetime.strftime("%Y-%m-%dT%H:%M:%S")
-
-#     return render_template('employee/employee-dashboard.html', progress=total_correct, total_questions=total_questions, quizzes=quizzes, percent=percent, num_correct=num_correct, num_incorrect=num_incorrect, quiz_list=quiz_list, current_date = formatted_current_datetime)
-
-# @session_handling_bp.route('/dashboard', methods=['GET', 'POST'])
-# def authenticate_user():
-#     msg = ''
-#     # Check if "username" and "password" POST requests exist (user submitted form)
-#     account = None
-#     cursor = database.conn.cursor()
-#     if 'logged_in' in session and session['logged_in']:
-#         cursor.execute('SELECT * FROM Users WHERE Username=? AND Password=?', (session['username'], session['password']))
-#         account = cursor.fetchone()
-#         if account and account[6] == 1:
-#             return render_template('manager/manager-dashboard.html')
-#         else:
-#             print("Back to dashboard")
-#             cursor.execute('SELECT IS_COMPLETED FROM USERS WHERE ID=? ', (session['id'],))
-#             query = cursor.fetchone()
-#             is_completed = query[0]
-#             if is_completed == 0:
-#                 certificate.generate_certificate()
-#             return render_employee_dashboard(account, cursor)
-
-#     # Show the login form with message (if any)
-#     return redirect(url_for('login'))
-
+# Verify the role of the user and redirect them to their respective
+# dashboard
 @auth_bp.route('/verify-role', methods=['GET', 'POST'])
 def verify_role():
+
+    # Check if the user is logged in
     if session['logged_in'] == True:
+
+        # Return them to their respective dashboard based on their role
         if session['role'] == Role.MANAGER.value:
             return redirect(url_for('manager.authenticate_manager'))
         else:
